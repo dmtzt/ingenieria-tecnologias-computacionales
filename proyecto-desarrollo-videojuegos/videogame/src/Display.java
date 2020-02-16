000e3d79019bc3c88f48238f41d4b9ff123e9f9d
David Martinez <34782922+dmtzt@users.noreply.github.com>
2020-02-15 20:35:11 -0600

    Proyecto inicial de clase de videojuegos

---
+++ proyecto-desarrollo-videojuegos/videogame/src/Display.java
@@ -0,0 +1,72 @@
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author david
+ */
+public class Display {
+
+    private JFrame jframe;
+    private Canvas canvas;
+
+    private String title;
+    private int width;
+    private int height;
+
+    /**
+     * Compilador de comentarios Genera un HTML de la clase que permite dar a
+     * conocer el proyecto Se pueden incluir etiquetas HTML
+     *
+     * Se agregan los comentarios que se encuentran arriba de la clase y
+     * de la definición de las funciones
+     * 
+     * @param title to display the title of the window
+     * @param width to set the width
+     * @param height to set the height
+     */
+    public Display(String title, int width, int height) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+        createDisplay();
+    }
+
+    public void createDisplay() {
+        // crate the app window
+        jframe = new JFrame(title);
+
+        // set the size of the window
+        jframe.setSize(width, height);
+
+        // setting not resizable, visible and possible to close
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setResizable(false);
+        jframe.setLocationRelativeTo(null);
+        jframe.setVisible(true);
+
+        // creating the canvas to paint and setting size
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(width, height));
+        canvas.setMaximumSize(new Dimension(width, height));
+        
+        // adding the canvas to the app window and packing to
+        // get the right dimensions
+        jframe.add(canvas);
+        jframe.pack();
+    }
+    /**
+     * Returns the canvas of the game
+     * @return the canvas
+     */
+    public Canvas getCanvas() {
+        return canvas;
+    }
+}
---
+++ proyecto-desarrollo-videojuegos/videogame/src/Game.java
@@ -0,0 +1,130 @@
+
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author david
+ */
+public class Game implements Runnable{
+    private BufferStrategy bs; // to have several buffers when displaying
+    private Graphics g; // to opaint objects
+    private Display display; // to display in the game
+    String title; // title of the window
+    private int width; // width of the window
+    private int height; // height of the window
+    private Thread thread; // thread to create the game
+    private boolean running; //to set the game
+    private int x = 0;
+    private int direction = 1;
+
+    @Override
+    public void run() {
+        System.out.println("Entrando al run");
+        // The IDE creates a default exception when the method is added
+        // The exception must be erased and the method completed
+        init();
+        
+        int fps = 50;
+        
+        double timeTick = 1000000000 / fps;
+        
+        double delta = 0;
+        
+        long now;
+        
+        long lastTime = System.nanoTime();
+        System.out.println("Valor de running: " + running);
+        while (running) {
+            now = System.nanoTime();
+            
+            delta += (now - lastTime) / timeTick;
+            
+            lastTime = now;
+        
+            if (delta >= 1) {
+                tick();
+                render();
+                delta--;
+            }
+        }
+        stop();
+    }
+    
+    public void tick() {
+        x += 1 * direction;
+        
+        if (x + 60 >= width) {
+            x = width - 60;
+            direction = -1;
+        }
+        else if (x <= 0) {
+            x = 0;
+            direction = 1;
+        }
+    }
+    /**
+     * 
+     * @param title
+     * @param width
+     * @param height 
+     */
+    // Variables must be initialized in the constructor or an init() method
+    public Game(String title, int width, int height) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+        running = false;
+    }
+    
+    private void init() {
+        display = new Display(title, width, height);
+        Assets.init();
+    }
+    
+    private void render() {
+        System.out.println("Entrando al render");
+        bs = display.getCanvas().getBufferStrategy();
+        
+        if (bs == null) {
+            display.getCanvas().createBufferStrategy(3);
+        }
+        else
+        {
+            g = bs.getDrawGraphics();
+            g.drawImage(Assets.background, 0, 0, width, height, null);
+            g.drawImage(Assets.player, x, height - 200, 200, 200, null);
+            bs.show();
+            g.dispose();
+        }
+    }
+    
+    // A synchronized method cannot be running at the same time than
+    // another synchronized method
+    public synchronized void start() {
+        System.out.println("Estoy en el start");
+        if (!running) {
+            running = true;
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+    
+    public synchronized void stop() {
+        if (running) {
+            running = false;
+            try {
+                thread.join();
+            }
+            catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+        }
+    }
+}
