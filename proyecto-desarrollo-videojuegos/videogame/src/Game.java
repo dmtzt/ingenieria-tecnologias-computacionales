b468dc7a0811f17e499017cca733e87e4a82ab19
David Martinez <34782922+dmtzt@users.noreply.github.com>
2020-02-26 16:36:20 -0600

    Revert "Enemy"
    This reverts commit 3b25725a1e4e6b826a440604eda920b4fc5b7605.

--- proyecto-desarrollo-videojuegos/videogame/src/Game.java
+++ proyecto-desarrollo-videojuegos/videogame/src/Game.java
@@ -1,7 +1,6 @@
 
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
-import java.util.LinkedList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -23,7 +22,6 @@ public class Game implements Runnable {
     private Thread thread; // thread to create the game
     private boolean running; //to set the game
     private Player player;
-    private LinkedList<Enemy> enemies;
     private KeyManager keyManager;
 
     @Override
@@ -62,15 +60,6 @@ public class Game implements Runnable {
     public void tick() {
         keyManager.tick();
         player.tick();
-        for (Enemy enemy : enemies) {
-            enemy.tick();
-            if (player.collision(enemy)) {
-                player.setX(0);
-                player.setY(getHeight() - 100);
-                enemy.setX((int) (Math.random() * getWidth() - 100));
-                enemy.setY(0);
-            }
-        }
     }
 
     /**
@@ -92,13 +81,6 @@ public class Game implements Runnable {
         display = new Display(title, getWidth(), getHeight());
         Assets.init();
         player = new Player(0, getHeight() - 100, 1, 100, 100, this);
-        enemies = new LinkedList();
-        int random = (int) (Math.random() * 6) + 3;
-        for (int i = 1; i <= random; i++) {
-            Enemy enemy = new Enemy((int) (Math.random() * getWidth()));
-            enemies.add(enemy);
-        }
-
         display.getJframe().addKeyListener(keyManager);
     }
 
@@ -111,12 +93,8 @@ public class Game implements Runnable {
             g = bs.getDrawGraphics();
             g.drawImage(Assets.background, 0, 0, width, height, null);
             player.render((g));
-            for (Enemy enemy : enemies) {
-                enemy.render((g));
-            }
             bs.show();
             g.dispose();
-    
         }
     }
 
